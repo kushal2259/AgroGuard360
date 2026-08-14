5a96f596efb7a6582a72cf0ef2227cf4d3c1df90
kushal2259 <kushalshukla5307@gmail.com>
2026-08-15 00:50:55 +0530

    Remove CustomCursor and restore normal system cursor

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { slides } from './data/slides.js'
 import Navigation from './components/Navigation.jsx'
 import LiveDemo from './components/LiveDemo.jsx'
 import MegaMenu from './components/MegaMenu.jsx'
-import CustomCursor from './components/CustomCursor.jsx'
 
 const slideVariants = {
   enter: (direction) => ({
@@ -64,7 +63,6 @@ export default function App() {
 
   return (
     <div className="relative h-screen w-full overflow-hidden bg-charcoal-950 text-mist-100">
-      <CustomCursor />
       <MegaMenu onGoTo={goTo} />
 
       <div className="absolute inset-0 pt-[72px]">
